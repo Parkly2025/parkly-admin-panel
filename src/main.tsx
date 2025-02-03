61d60d0a5f2b6b72cdc6f1312d242868778f9e12
Jyrko <uriydemoshenko@gmail.com>
2025-02-03 16:04:09 +0100

    login page, navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import App from '@/app/dashboard/page.tsx'
+import ComingSoon from './components/coming-soon.tsx';
+import { LoginForm } from '@/components/login-form.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginForm />}/>
+          <Route path="/" element={<App />}/>
+          <Route path="*" element={<ComingSoon />}/>
+        </Routes>
+    </BrowserRouter>,
+  </StrictMode>
 )
