255a96419f5570667fe741f6a03e2a13cfaa4aa5
Jyrko <uriydemoshenko@gmail.com>
2025-02-04 14:41:55 +0100

    404 page

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,22 @@ import './index.css'
 import AuthentificatedLayout from '@/components/layout/authentificated-layout.tsx'
 import Dashboard from "@/app/dashboard/page.tsx"
 // import Payments from "@/app/payments/page.tsx"
-import ComingSoon from './components/coming-soon.tsx';
+import ComingSoon from './components/coming-soon.tsx'
 import { LoginForm } from '@/components/login-form.tsx'
+import NotFoundError from '@/features/errors/not-found-error'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
         <Routes>
+          <Route index element={<ComingSoon />}/>
           <Route path="/login" element={<LoginForm />}/>
           <Route path="/dashboard/" element={<AuthentificatedLayout />}>
             <Route index element={<Dashboard />} />
             {/* <Route path="/dashboard/payments/" element={<Payments />} /> */}
           </Route>
-          <Route path="*" element={<ComingSoon />}/>
+          <Route path="*" element={<NotFoundError/>}/>
         </Routes>
-    </BrowserRouter>,
+    </BrowserRouter>
   </StrictMode>
 )
